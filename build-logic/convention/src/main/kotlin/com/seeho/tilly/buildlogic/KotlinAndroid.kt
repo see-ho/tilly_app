860c863bef8dd9b48c76a34019e1fe50ce3c4600
seeho <siki7878@gmail.com>
2026-02-06 21:44:17 +0900

    Chore: 자바 및 코틀린 버전 17로 업데이트
    *   `build-logic` 내 `KotlinAndroid` 컨벤션 플러그인에서 사용하는 자바 및 코틀린 버전을 11에서 17로 상향 조정했습니다.

--- build-logic/convention/src/main/kotlin/com/seeho/tilly/buildlogic/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/seeho/tilly/buildlogic/KotlinAndroid.kt
@@ -15,12 +15,12 @@ internal fun Project.configureKotlinAndroid(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
         kotlinOptions {
-            jvmTarget = "11"
+            jvmTarget = "17"
         }
     }
 }
