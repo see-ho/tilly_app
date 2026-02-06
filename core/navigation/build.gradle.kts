cd0cc663c7bdba30a9c5f009615256295c0f0689
seeho <siki7878@gmail.com>
2026-02-06 21:53:10 +0900

    Refactor: 네비게이션 모듈 Gradle 설정 및 의존성 추가
    * `core/navigation` 모듈에 Compose 및 Kotlin Serialization 플러그인을 적용했습니다.

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
     id("tilly.android.library")
+    id("tilly.android.compose")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.seeho.tilly.core.navigation"
+}
+
+dependencies {
+    api(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 }
