7d4066e3b43fe2ab4c465b069fe8abde1c930767
seeho <siki7878@gmail.com>
2026-02-10 23:38:01 +0900

    Feat: 네트워크 모듈 설정 및 Retrofit 종속성 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,13 +1,40 @@
+import java.util.Properties
+
 plugins {
     id("tilly.android.library")
     id("tilly.android.hilt")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.seeho.tilly.core.network"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        val properties = Properties()
+        val localPropertiesFile = project.rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            properties.load(localPropertiesFile.inputStream())
+        }
+
+        buildConfigField(
+            "String",
+            "OPENAI_API_KEY",
+            "\"${properties.getProperty("OPENAI_API_KEY") ?: ""}\""
+        )
+    }
 }
 
 dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:common"))
+    
+    // Networking
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
 }
