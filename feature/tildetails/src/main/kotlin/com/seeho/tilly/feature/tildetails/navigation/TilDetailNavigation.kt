3c7488574cf3f804850688cea2002b38e28cc39d
seeho <siki7878@gmail.com>
2026-02-11 11:22:12 +0900

    Feat: TIL 저장 후 상세 화면 진입 시 스낵바 표시 기능 추가

--- feature/tildetails/src/main/kotlin/com/seeho/tilly/feature/tildetails/navigation/TilDetailNavigation.kt
+++ feature/tildetails/src/main/kotlin/com/seeho/tilly/feature/tildetails/navigation/TilDetailNavigation.kt
@@ -3,6 +3,7 @@ package com.seeho.tilly.feature.tildetails.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.seeho.tilly.core.navigation.TilDetail
 import com.seeho.tilly.feature.tildetails.TilDetailScreen
 
@@ -14,10 +15,12 @@ fun NavGraphBuilder.tilDetailScreen(
     onEditClick: (Long) -> Unit,
 ) {
     composable<TilDetail> { backStackEntry ->
+        val route = backStackEntry.toRoute<TilDetail>()
         TilDetailScreen(
             onBackClick = onBackClick,
             onDeleteClick = onDeleteClick,
             onEditClick = onEditClick,
+            fromSave = route.fromSave,
         )
     }
 }
