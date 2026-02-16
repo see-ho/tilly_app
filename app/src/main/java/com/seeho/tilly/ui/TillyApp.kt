337536e65bb7ef5bf1eaba636bd9aeeb43606c19
seeho <siki7878@gmail.com>
2026-02-16 11:38:34 +0900

    Feat: TopLevelDestination에 따라 TopAppBar 제목 변경

--- app/src/main/java/com/seeho/tilly/ui/TillyApp.kt
+++ app/src/main/java/com/seeho/tilly/ui/TillyApp.kt
@@ -39,8 +39,15 @@ fun TillyApp(
 ) {
     Scaffold(
         topBar = {
-            if (appState.currentTopLevelDestination != null) {
+            val topLevelDestination = appState.currentTopLevelDestination
+            if (topLevelDestination != null) {
                 TillyTopAppBar(
+                    titleText = when (topLevelDestination) {
+                        TopLevelDestination.STATISTICS -> "Statistics"
+                        TopLevelDestination.HOME -> "TILLY"
+                        TopLevelDestination.REPORT -> "Report"
+                        else -> "TILLY"
+                    },
                     onSettingsClick = { /* TODO: 설정 화면*/ }
                 )
             }
