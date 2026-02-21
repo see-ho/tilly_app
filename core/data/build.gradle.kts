e2f939390a43cb11d0cb628a25cbd75cf36fb98e
seeho <siki7878@gmail.com>
2026-02-21 23:56:42 +0900

    Refactor: 불필요한 의존성 제거

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -13,5 +13,4 @@ dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:database"))
     implementation(project(":core:network"))
-    implementation(libs.room.ktx)
 }
