d3eee8845093ac1b69b5a0bbf48864031f6da1a5
seeho <siki7878@gmail.com>
2026-02-24 11:52:30 +0900

    Refactor: Report 모듈 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
-plugins {
+ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.aboutlibraries)
 }
 
 android {
@@ -47,7 +48,6 @@ dependencies {
     implementation(project(":feature:tildetails"))
     implementation(project(":feature:shop"))
     implementation(project(":feature:mypage"))
-    implementation(project(":feature:report"))
     implementation(project(":feature:statistics"))
     implementation(project(":feature:editor"))
     implementation(project(":core:designsystem"))
@@ -67,6 +67,9 @@ dependencies {
     
     implementation(libs.coil.compose)
     implementation(libs.coil.gif)
+
+    implementation(libs.aboutlibraries.core)
+    implementation(libs.aboutlibraries.compose.m3)
 
     implementation(libs.hilt.android)
     implementation(libs.androidx.navigation.compose)
